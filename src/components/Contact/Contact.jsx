434741c9f1b73c8408f41653aa98abb37941e983
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-12-01 12:07:41 +0100

    com011224-1207

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,14 @@
 import s from './Contact.module.css'
+import { AiOutlineUser } from "react-icons/ai";
+import { AiTwotonePhone } from "react-icons/ai";
 
 
 const Contact = ({ data: {id, name, number}, onDelete }) => {
     return (
         <div className={s.wrapper}>
             <div className={s.datacontainer}>
-                <p> {name}</p>
-                <p> {number}</p>
+                <p><AiOutlineUser />{name}</p>
+                <p><AiTwotonePhone /> {number}</p>
             </div>
             
             <button className={s.btn} type="submit" onClick={()=>onDelete(id)}>Delete</button>
